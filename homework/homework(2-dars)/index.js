0793c0ad877089b65d087216fe33aa1b631d070a
A-Muhammadjon <muhammadjonabduhomidov4@gmail.com>
2025-04-09 14:40:13 +0500

    JavaScript

---
+++ homework/homework(2-dars)/index.js
@@ -0,0 +1,139 @@
+// let a = 1, b = 1;
+// let c = ++a; j:2
+// let d = b++; j:1
+
+
+
+
+
+// let a = 2;
+// let x = 1 + (a *= 2)  j:5
+
+
+
+
+// console.log("" + 1 + 0);
+//  j:10
+
+// console.log("" - 1 + 0);
+//  j:-1
+
+// console.log(true + false);
+//  j:true(1)
+
+// console.log(6 / "3"); 
+// J;2
+
+// console.log("2" * "3");
+//  j:6
+
+// console.log(4 + 5 + "px");
+// j:9px
+
+// console.log("$" + 4 + 5);
+// j:$45
+
+// console.log("4" - 2);
+//  j:2
+
+// console.log("4px" - 2);
+// j:Nan
+
+// console.log(" -9 " + 5);
+// j:-95
+
+// console.log(" -9 " - 5);
+// j:-14
+
+// console.log(null + 1);
+// j:1
+
+// console.log(undefined + 1);
+// j:Nan
+
+// console.log(" \t \n" - 2);
+// j:-2
+
+
+
+
+
+
+
+// console.log(5 > 4); 
+// j:true
+
+// console.log("apple" > "pineapple");
+//  j:false
+
+// console.log("2" > "12");
+// j:true
+
+// console.log(undefined == null);
+// j:true
+
+// console.log(undefined === null); 
+// j:false
+
+// console.log(null == "\n0\n"); 
+// j:false
+
+// console.log(null === +"\n0\n");
+//  j:false
+
+
+
+
+
+
+
+
+
+
+// alert( null  || 2 || undefined); 
+// j:2
+
+
+
+
+
+
+
+
+
+
+// alert(alert(1) || 2 || alert(3));
+//  j:1,2
+
+
+
+
+
+
+
+
+
+
+// alert(1 && null && 2); 
+// j:null
+
+
+
+
+
+
+// alert(alert(1) && alert(2)); j : 1, underfine
+
+
+
+
+
+
+
+
+
+
+
+
+// alert( null || 2 && 3 || 4);
+//  j: 3
