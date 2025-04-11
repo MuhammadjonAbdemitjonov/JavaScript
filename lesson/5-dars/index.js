c4c01d77b6459d48007c1c4e1b14c1fc9372ce51
A-Muhammadjon <muhammadjonabduhomidov4@gmail.com>
2025-04-11 17:54:17 +0500

    dars-5

---
+++ lesson/5-dars/index.js
@@ -0,0 +1,23 @@
+// let a = 123
+// a % 2 ? ++a : --a;
+// console.log(a);
+
+// // if(a%2){
+// //     console.log("toq");
+// // }
+// // else{
+// //     console.log("juft");
+
+// // }
+
+let a = 65,
+  b = 5;
+a > b ? console.log(a, b) : console.log(b, a);
+// if (a>b) {
+//     console.log(a);
+//     console.log(b);
+// }
+// else{
+//     console.log(b);
+//     console.log(a);
+// }
