90d81a0efdc1a602d0f44e95ffac0cbabbfa236a
Muhammadjon <muhammadjonabduhomidov4@gmail.com>
2025-04-18 14:54:29 +0500

    6-dars

---
+++ homework/homework(6-dars)/index.js
@@ -0,0 +1,30 @@
+// let i = 1
+// let hisob = 0
+// while(i <= 5){
+//     let son = Number(prompt("xoxlagan sonni kiriting"))
+//     hisob = hisob + son
+//     let arifmetika = hisob / i
+//     console.log(`umumiy son ${hisob} uning arifmetik amali esa ${arifmetika}`);
+//     i++
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let n = 2
+// let hisob = 1
+// while(n <= 5){
+//     let sum = Number(prompt("ixtiyoriy son"))
+//     hisob = hisob * sum
+//     let natija = hisob ** (1/n)
+//     console.log(natija);
+//     n++
+// }
