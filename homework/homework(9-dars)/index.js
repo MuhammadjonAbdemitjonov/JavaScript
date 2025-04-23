37f5cdd3301585446984ffb0e2bfe3881f25792a
A-Muhammadjon <muhammadjonabduhomidov4@gmail.com>
2025-04-23 13:51:20 +0500

    homework

---
+++ homework/homework(9-dars)/index.js
@@ -0,0 +1,39 @@
+let data = {
+    name: "Phone",
+    model: "iPhone",
+    price: 700,
+    quantity: 8
+};
+
+let umumiy = data.price * data.quantity
+console.log(`umumiy hisob ${umumiy}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let student = {
+    firstName: "Eshmat",
+    lastName: "Toshmatov",
+    age: 20,
+    course: "Frontend"
+};
+
+console.log(`${student.firstName} ${student.lastName} ${student.age}`);
