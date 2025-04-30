b048716bb52c02e9a26c41000793e72bf43d1854
Muhammadjon <muhammadjonabduhomidov4@gmail.com>
2025-04-30 14:11:07 +0500

    homewrok

---
+++ homework/homeworl(12-dars)/index.js
@@ -0,0 +1,41 @@
+
+// let user = {
+//     id: 1,
+//     firstName: "Eshmat",
+//     surName: "Toshmatov",
+//     age: 30,
+//     getFullName: () => {
+//       return `${this.firstName} ${this.surName}`;
+//     },
+//   };
+  
+//   console.log(user.getFullName());
+// //   natija : undefined,  undefined , sababbi shundaki bu yerda this userga emas globalga qarayapti yani this.firstName da userdagi firstname dan emas globaldan qidiryapti chunki  arraw function this ni oziga boglamaydi
+
+
+
+
+
+
+
+
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep() {
+      console.log(this.step);
+      return this;
+    },
+  };
+  
+  ladder.up().up().down().showStep().down().showStep();
+
+  
+//   natija : 1, 0 ,     chunki birinchi qadamda yani up da step 1 ga teng boladi  keyin 1 yana oshadi va 2 boladi keyin down -1 qiladi va step qiymati 1 boladi keyin showstep kelib console.log qiladi va consolga  1 chiqadi undan song down yana stepni 1 qiymatidan -1 qiladi va step qiymati 0 boladi yana showstep keladi consol.log qiladi  va consolga 0 chiqadi
