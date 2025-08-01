c718c1010e7b1b016adf5052729bb246a0a2c696
A-Muhammadjon <muhammadjonabduhomidov4@gmail.com>
2025-08-01 16:12:45 +0500

    push

--- homework/homework(39-dars)/index.js
+++ homework/homework(39-dars)/index.js
@@ -1,51 +1,42 @@
-// function* GetVal(id) {
-//   while (1) {
-//     yield id++;
-//   }
-// }
+function* GetVal(id) {
+  while (1) {
+    yield id++;
+  }
+}
 
-// let obj = GetVal(1);
+let obj = GetVal(1);
 
-// let arr = [
-//   { id: obj.next().value, name: `Eshmat` },
-//   { id: obj.next().value, name: `Oxford` },
-// ];
+let arr = [
+  { id: obj.next().value, name: `Eshmat` },
+  { id: obj.next().value, name: `Oxford` },
+];
 
-// let list = document.querySelector("#list");
+let list = document.querySelector("#list");
 
-// function SetUser() {
-//   list.innerHTML = "";
-//   arr.forEach((obj) => {
-//     let h1 = document.createElement("h1");
-//     h1.innerHTML = `${obj?.name} - ${obj?.id} <button onclick="del(${obj?.id})">Delete</button>`;
-//     list.append(h1);
-//   });
-// }
+function SetUser() {
+  list.innerHTML = "";
+  arr.forEach((obj) => {
+    let h1 = document.createElement("h1");
+    h1.innerHTML = `${obj?.name} - ${obj?.id} <button onclick="del(${obj?.id})">Delete</button>`;
+    list.append(h1);
+  });
+}
 
-// function Yana() {-
-//   let a = document.querySelector("#inp");
-//   return a.value;
-// }
+function Yana() {-
+  let a = document.querySelector("#inp");
+  return a.value;
+}
 
-// function AddUser() {
-//   let val = Yana();
-//   arr.push({ id: obj.next().value, name: val });
-//   SetUser();
-// }
+function AddUser() {
+  let val = Yana();
+  arr.push({ id: obj.next().value, name: val });
+  SetUser();
+}
 
-// let del = (id) => {
-//   arr = arr.filter((obj) => obj.id !== id);
-//   SetUser();
-// };
-
-// SetUser();
-// -----------------------------
-let getval = (a) => {
-  return (b) => {
-    if (b) {
-      return getval(a + b);
-    }
-  };
-  return a;
+let del = (id) => {
+  arr = arr.filter((obj) => obj.id !== id);
+  SetUser();
 };
-console.log(getval(1)(2)(3)(4)());
+
+SetUser();
+
